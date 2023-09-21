cb155ebc6b3b6aae845e8cca77e3829dbcf9c344
Anna Luísa <annalf1401@gmail.com>
2023-09-21 11:02:23 -0300

    Atualização de definições.

--- src/fidelidade_quantica.py
+++ src/fidelidade_quantica.py
@@ -1,22 +1,23 @@
 import numpy as np
 import quantum_gaussian_toolbox as qgt
 
-vac = qgt.vacuum()  # Variável para estado no vácuo;
-coh = qgt.coherent(1 - 20j)  # Variável para estado coerente;
-sq = qgt.squeezed(1.2)  # Estado comprimido
-th = qgt.thermal(4)  # Estádo térmico
+# aplicar uma condicao para a matriz, pag 33. Tem que ser simetrica
+# forma simplética. conferir se é fisicamente aceitavel
 
-R = np.array([1, 2, 3, 4])  # Vetor médio de quadratura
-M = np.eye(4)  # Matriz de covariância | Definindo matriz identidade 4x4
+R = np.array([1, 2, 3, 4])  # Vetor médio de quadratura para o estado 0, sempre positivo
+M = np.eye(4)  # Matriz de covariancia | Definindo matriz identidade 4x4
 
-V = 10 * M
-alpha = 1 - 2.0j  # Representação de um número complexo.
+V = 10 * M  # Matriz de covariancia para o estado 0
+alpha = 1 - 2.0j  # Representacao de um numero complexo.
 
-state_0 = qgt.gaussian_state(R, V)  # Estado Gaussiano Multimodo
+estado_0 = qgt.gaussian_state(R, V)  # Estado Gaussiano Multimodo
 
-state_1 = qgt.coherent(alpha)
+estado_1 = qgt.coherent(alpha)
 
-state_2 = qgt.tensor_product([state_1, state_1])
-state_1.tensor_product([state_1])
+estado_2 = qgt.tensor_product([estado_1, estado_1])
+estado_1.tensor_product([estado_1])
 
-F = qgt.fidelity(state_0, state_1)
+F = qgt.fidelity(estado_0, estado_1)
+
+print(F)
+
--- src/quadrature_dynamics.py
+++ src/quadrature_dynamics.py
@@ -1,2 +1,15 @@
 import numpy as np
 import quantum_gaussian_toolbox as qgt
+import matplotlib.pyplot as plt
+
+# Parâmetros para a dinâmica
+omega = 2 * np.pi  # Frequência natural [Hz]
+t = np.linspace(0, 2 / omega,
+                int(200))  # Variavel para acompanhar a evolução de um sistema ou processo ao longo do tempo.
+
+# Matriz de derivação
+A = np.array([[0, -omega],
+              [omega, 0]])
+
+D = np.diag([0, 0])  # Matriz de difusão
+N = np.zeros((2, 1))  # Vetor de conducao
