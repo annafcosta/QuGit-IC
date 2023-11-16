85f2664db4b19d6ee52128ef81e41c46b5ae016d
Anna Luísa <annalf1401@gmail.com>
2023-11-16 09:17:30 -0300

    Atualização de definições e restrições.

--- src/fidelidade_quantica.py
+++ src/fidelidade_quantica.py
@@ -1,34 +1,45 @@
 import numpy as np
 import quantum_gaussian_toolbox as qgt
 
-# Verificar se a matriz resultante é não negativa definida (σ >= 0):
-def check_condition(matriz):
-    eigenvalues = np.linalg.eigvalsh(matriz) # Calcular os autovalores de uma matriz simétrica
-    return all(eig >= 0 for eig in eigenvalues) # Verificar se todos os autovalores são não negativos
+# Verificar se a matriz resultante é não negativa definida (σ + iΩ ≥ 0):
+def check_condition(matrix):
+    matrix_real = matrix.real
+    eigenvalues = np.linalg.eigvals(matrix_real)
+    return all(eig >= 0 for eig in eigenvalues)
 
-# Exemplo de uso:
-sigma = np.eye(4)  # Matriz de covariancia | Definindo matriz identidade 4x4
 
+# Aplicação para matrizes aleatórias:
+sigma = np.random.rand(4, 4)  # Matriz de covariancia
+print(sigma)
 sigma_cov = 10 * sigma  # Matriz de covariancia para o estado 0 (apenas parte real)
 
 if check_condition(sigma_cov):
-    print("A condição é satisfeita.")
+    print("\nA condição é satisfeita.")
 else:
-    print("A condição não é satisfeita.")
+    print("\nA condição não é satisfeita.")
 
-# Fidelidade Quântica
 
-R = np.array([1, 2, 3, 4])  # Vetor médio de quadratura para o estado 0, sempre positivo
-alpha = 1 - 2.0j  # Representacao de um numero complexo.
+def main():
+    # Fidelidade Quântica
 
-# Definição dos estados:
+    R = np.array([1, 2, 3, 4])  # Vetor médio de quadratura para o estado 0, sempre positivo
+    alpha = 1 - 2.0j  # Representacao de um numero complexo.
 
-estado_0 = qgt.gaussian_state(R, sigma_cov)  # Estado Gaussiano Multimodo
-estado_1 = qgt.coherent(alpha)
+    # Definição dos estados:
 
-estado_2 = qgt.tensor_product([estado_1, estado_1])
-estado_1.tensor_product([estado_1])
+    estado_0 = qgt.gaussian_state(R, sigma_cov)  # Estado Gaussiano Multimodo
+    estado_1 = qgt.coherent(alpha)
 
-F = qgt.fidelity(estado_0, estado_1)
+    if not check_condition(sigma_cov):
+        print("Não é possível calcular a fidelidade.")
+        return
 
-print(F)
+    estado_2 = qgt.tensor_product([estado_1, estado_1])
+    estado_1.tensor_product([estado_1])
+
+    F = qgt.fidelity(estado_0, estado_2)
+
+    print(F)
+
+
+main()
